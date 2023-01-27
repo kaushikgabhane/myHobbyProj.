e5a7235d359e068e40c2213be4186cd3a67ee83e
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-01-27 11:09:04 +0530

    Calculator project complete

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,10 +1,29 @@
-let screen = document.querySelector(".screen");
-let value = document.createElement("h4");
 
-console.log(screen);
+// console.log(screen);
 
-const btn1 = document.querySelector(".btn-1");
-btn1.addEventListener("click", () => {
-    value.innerText = "1";
-    screen.appendChild(value);
+const btn1 = document.querySelectorAll(".calc-btns");
+const arr = [...btn1];
+let value = "";
+const  screen = document.querySelector('.screen');
+
+arr.forEach((btn) => {
+   
+    btn.addEventListener("click", (e) => {
+        let x = e.target;
+        if (x.innerText == '=') {
+            value = eval(value);
+            console.log(value);
+            screen.innerHTML = value;
+        }
+        else if (x.innerText == 'AC') {
+            screen.innerHTML = " ";
+            value = " ";
+        }
+        else{
+            value = value + x.innerText;
+            // console.log(value);
+            screen.innerText = value;
+
+        }
+    });
 });
